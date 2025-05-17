67b464923e1eb5db4bdf2a6cb7bda483e7c03bf4
roshangautam <roshangautam53@hotmail.com>
2025-05-17 19:46:17 -0400

    Implemnted insertion sort in decreasing order

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,14 @@
 #Python program for the Insertion Sort Algorithm that sorts in monotonically decreasing order.
 
+#Function to sort in descending order using Insertion Sort
 def insertion_sort_in_dec_order(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
     return arr
 
 if __name__ == "__main__":
