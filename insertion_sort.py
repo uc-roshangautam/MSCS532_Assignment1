de4849347e2be2306d789d24980c0f2db3657389
roshangautam <roshangautam53@hotmail.com>
2025-05-17 19:02:12 -0400

    Setting up function and io handling

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,1 +1,9 @@
 #Python program for the Insertion Sort Algorithm that sorts in monotonically decreasing order.
+
+def insertion_sort_in_dec_order(arr):
+    return arr
+
+if __name__ == "__main__":
+    data = [5, 2, 9, 9, 3, 6]
+    sorted_data = insertion_sort_in_dec_order(data)
+    print("Sorted in decreasing order:", sorted_data)
